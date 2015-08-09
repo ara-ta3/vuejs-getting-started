0f9cc884a8fc76ef441181dfb2ac3b377e040ed7
Arata Tanaka <tarata43@yahoo.co.jp>
2015-08-09 22:08:22 +0900

    add power assert

--- test/PastRecordTest.js
+++ test/PastRecordTest.js
@@ -1,12 +1,27 @@
-var assert = require('assert');
+var assert = require('power-assert');
 var PastRecord = require('../src/gacha/PastRecord.js');
 
 describe('PastRecord', function() {
     describe('history', function() {
-        it('空文字列が返ること', function() {
+        it('初期では空文字列が返ること', function() {
             var record = new PastRecord();
-            assert.equal(record.history(), '');
+            var actual = record.history().toString();
+            assert.equal(actual, '');
+        });
+
+        it('1つ追加された時, その文字列が返ること', function() {
+            var record = new PastRecord();
+            record.appendResult("hoge");
+            var actual = record.history().toString();
+            assert.equal(actual, 'hoge');
+        });
+
+        it('2つ追加された時, その文字列がカンマ区切りで返ること', function() {
+            var record = new PastRecord();
+            record.appendResult("hoge");
+            record.appendResult("fuga");
+            var actual = record.history().toString();
+            assert.equal(actual, 'hoge,fuga');
         });
     });
-
 });

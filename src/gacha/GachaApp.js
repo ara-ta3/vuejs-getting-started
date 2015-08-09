ccf66887e7e5feb670f1714084bc846b59214efc
Arata Tanaka <tarata43@yahoo.co.jp>
2015-08-09 23:13:38 +0900

    appを閉じ込めた

---
+++ src/gacha/GachaApp.js
@@ -0,0 +1,36 @@
+module.exports = (function() {
+    var Vue = require('vue');
+
+    var Gacha = require("./Gacha.js");
+
+    var PastRecord  = require("./PastRecord.js");
+
+    var Master = require("./Master.js");
+
+    var rateOneData  = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    var master      = new Master(rateOneData);
+    master.appendData(["A","B","C"], 5);
+
+    return new Vue({
+        el: '#app',
+        data: {
+            gacha: new Gacha(master.getGachaItems()),
+            record: new PastRecord(master.getAllData()),
+            item: "Item",
+            history: "History",
+            rest: "Rest Items"
+        },
+        created: function() {
+            this.rest = this.record.rest();
+        },
+        methods: {
+            draw: function() {
+                var item        = this.gacha.draw();
+                this.item       = item;
+                this.record.appendResult(item);
+                this.rest       = this.record.rest();
+                this.history    = this.record.history();
+            }
+        }
+    });
+})();
